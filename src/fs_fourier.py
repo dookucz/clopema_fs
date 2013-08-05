9489c67926b36e2179c2a6ef2d29657b60f5ab1a
dookucz <dooku.cz@gmail.com>
2013-08-05 10:57:25 +0000

    fourier transformation of z-axis sensor data: fs_fourier.py

---
+++ src/fs_fourier.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2
+import re
+import pyqtgraph as pg
+import numpy
+import math
+import scipy
+import sys
+from PyQt4 import QtGui,QtCore
+import roslib; roslib.load_manifest('clopema_calibration')
+import rospy, smach, smach_ros, copy, tf, PyKDL, os, shutil
+from tf.transformations import quaternion_from_euler, quaternion_about_axis
+from tf_conversions import posemath
+from clopema_smach import *
+from geometry_msgs.msg import *
+from smach import State
+from clopema_planning_actions.msg import MA1400JointState
+
+def netf_cb(msg):
+	global FREQ_CONST
+	global td_data
+	global td_curve
+	global fd_curve
+	
+	if len(td_data) == FREQ_CONST:
+		td_curve.setData(td_data)
+		fd_curve.setData(numpy.fft.rfft(td_data).real)
+		td_data = []
+	else:
+		td_data.append(msg.wrench.force.z)
+
+def main():
+	global FREQ_CONST
+	global td_data
+	global td_curve
+	global fd_curve
+	FREQ_CONST = 7000
+	td_data = []
+	rospy.init_node('sm_test')
+	# init default data
+	i = 0
+	while i < FREQ_CONST:
+		td_data.append(0)
+		i += 1
+	win = pg.GraphicsWindow(title="Fourier transformation of force sensor signal")
+	win.resize(1000,600)
+	win.setWindowTitle('Fourier transformation of force sensor signal')
+	time_domain = win.addPlot(title = 'Z-axis time domain')
+	td_curve = time_domain.plot(td_data, pen = 'b')
+	win.nextRow()
+	freq_domain = win.addPlot(title = 'Z-axis freq domain')
+	fd_curve = freq_domain.plot(td_data, pen = 'b')
+
+	rospy.Subscriber('/netft_data',WrenchStamped,netf_cb)
+	QtGui.QApplication.instance().exec_()
+	rospy.spin()
+
+if __name__ == '__main__':
+	main()
