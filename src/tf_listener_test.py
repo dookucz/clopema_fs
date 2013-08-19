f2e779a6b8dbf8421a43fda2874d5018fe5b4bb7
Clopema Guest <clopema@clopema1.(none)>
2013-08-19 12:48:18 +0200

    minor changes

--- src/tf_listener_test.py
+++ src/tf_listener_test.py
@@ -3,9 +3,27 @@ import roslib; roslib.load_manifest('clopema_calibration')
 import rospy
 import math
 import tf
+import numpy
 from tf_conversions import posemath
 from geometry_msgs.msg import Pose
 from clopema_utilities import np_bridge
+
+def quat2rot(quat):
+	w = quat[3]
+	x = quat[0]
+	y = quat[1]
+	z = quat[2]
+	Nq = w*w + x*x + y*y + z*z
+	if Nq > 0.0:
+		s = 2/Nq
+	else:
+		s = 0.0
+	X = x*s; Y = y*s; Z = z*s
+	wX = w*X; wY = w*Y; wZ = w*Z
+	xX = x*X; xY = x*Y; xZ = x*Z
+	yY = y*Y; yZ = y*Z; zZ = z*Z
+	return numpy.matrix([[1.0-(yY+zZ), xY-wZ, xZ+wY],[xY+wZ,1.0-(xX+zZ),yZ-wX],[xZ-wY,yZ+wX,1.0-(xX+yY)]])
+	
 
 if __name__ == '__main__':
 	rospy.init_node('tf_listener_test')
@@ -15,4 +33,5 @@ if __name__ == '__main__':
 		listener.waitForTransform('/base_link','/r2_gripper',now,rospy.Duration(1.5))
 		(trans,rot) = listener.lookupTransform('/base_link','/r2_gripper',now)
 		print 'Trans = ' + str(trans) + ' rot = ' + str(rot)
+		print quat2rot(rot)
 		rospy.sleep(1)
