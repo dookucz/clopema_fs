03b4ae866320f25f17c0e355f5a701127de7aa35
dookucz <dooku.cz@gmail.com>
2013-08-20 11:05:12 +0000

    minor changes

--- src/centroid.py
+++ src/centroid.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 	global F,T
 
 	# number of readings to make
-	SENSOR_READINGS = 100 
+	SENSOR_READINGS = 4 
 
 	F = np.empty(shape = (3*SENSOR_READINGS,1))
 	T = np.empty(shape = (3*SENSOR_READINGS,1))
@@ -56,12 +56,13 @@ if __name__ == '__main__':
 		sys.exit(1)
 	except ROSException:
 		print 'Non-existent topic (try /netft_data)'
+		print 'USAGE: centroid.py NAME_OF_TOPIC'
 		sys.exit(1)
 
 	while not rospy.is_shutdown():
-		count = 0
 
 		# getting sensor data
+		count = 0
 		while count < SENSOR_READINGS:
 			rospy.spinOnce()
 			count += 1
@@ -75,11 +76,11 @@ if __name__ == '__main__':
 		# creating matrices for calculation
 		A = np.empty(shape=(3*SENSOR_READINGS,4))
 		i = 0
-		while i < 3*SENSOR_READINGS:
-			A[i] = np.matrix([[1,0,0,-R[0,2]*g]])
-			A[i+1] = np.matrix([A,[0,1,0,-R[1,2]*g]])
-			A[i+2] = np.matrix([A,[0,0,1,-R[2,2]*g]])
-			i += 3
+		while i < SENSOR_READINGS:
+			A[3*i] = np.matrix([[1,0,0,-R[0,2]*g]])
+			A[3*i+1] = np.matrix([A,[0,1,0,-R[1,2]*g]])
+			A[3*i+2] = np.matrix([A,[0,0,1,-R[2,2]*g]])
+			i += 1
 			
 		# calculating
 		U,s,V = np.linalg.svd(A,full_matrices=1)
@@ -90,4 +91,11 @@ if __name__ == '__main__':
 		S[2,2] = s[2]
 		S[3,3] = s[3]
 		X = V*np.linalg.pinv(S)*U.getH()*b
-		print 'X =\n' + str(X)
+		Fo = X[0:2]
+		m = X[3]
+
+		# printing output
+		print 'R =\n' + str(R)
+		print 'm = ' + str(m)
+		print 'Fo =\n' + str(Fo)
+		rospy.sleep(5)
