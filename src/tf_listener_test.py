69643967d5f00de2d013faf97098659809d78933
dookucz <dooku.cz@gmail.com>
2013-08-26 15:13:26 +0000

    minor changes

--- src/tf_listener_test.py
+++ src/tf_listener_test.py
@@ -2,7 +2,7 @@
 import roslib; roslib.load_manifest('clopema_fs')
 import rospy
 import math
-import tf2_ros
+import tf
 import numpy
 from tf_conversions import posemath
 from geometry_msgs.msg import Pose
@@ -27,19 +27,10 @@ def quat2rot(quat):
 
 if __name__ == '__main__':
 	rospy.init_node('tf_listener_test')
-	print rospy.rostime.is_rostime_initialized()
-	#rospy.sleep(500)
-	print 'pred listenerem'
-	tf_buffer = tf2_ros.Buffer()
-	listener = tf2_ros.TransformListener(tf_buffer)
-	print 'po listeneru'
+	listener = tf2_ros.TransformListener()
 	while not rospy.is_shutdown():
-		print 'pred rospy.Time(0)'
 		now = rospy.Time(0)
-		print 'po rospy.Time(0)'
-		print 'pred waitForTransform()'
 		listener.waitForTransform('/base_link','/r2_force_sensor',now,rospy.Duration(1.5))
-		print 'po waitForTransform()'
 		(trans,rot) = listener.lookupTransform('/base_link','/r2_force_sensor',now)
 		print str(quat2rot(rot)) + '\n\n'
 		rospy.sleep(3)
